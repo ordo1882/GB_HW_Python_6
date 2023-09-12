187d5fb4af6f95db7402786c7728846c384323c5
Oleg <ordo1882@gmail.com>
2023-09-13 01:39:32 +0200

    Added Task 1

---
+++ Task_1/main.py
@@ -0,0 +1,12 @@
+# Задача 30:  Заполните массив элементами арифметической прогрессии. 
+# Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+a1 = int(input("Enter the first number: "))
+d = int(input("Enter the step size: "))
+n = int(input("Enter the steps number: "))
+
+lst = [a1 if i == 0 else i * d + a1 for i in range(n)]
+
+print(*lst)
